70173fd288deaf7713cbf4257a1abfec21623480
majakochanowska <51491969+majakochanowska@users.noreply.github.com>
2020-05-26 15:44:58 +0200

    Add files via upload

---
+++ data.js
@@ -0,0 +1,66 @@
+data = [
+{
+    "date": "2007-10",
+    "content": "I got accepted on my desired field of study and I started my degree of Biotechnology at University of Warsaw."
+},
+{
+    "date": "2007-12",
+    "content": "On the first year I got interested in HTML and I thought it would be fun to make website about my experiences from Biotechnology studies. I started to learn on my own from internet and I made my first website. It's based on tables and it looks awful but it's still on the web ;) (<a href='http://biotech.freehost.pl' target='_blank'>biotech.freehost.pl</a>)"
+},
+{
+    "date": "2008-11",
+    "content": "On the 2nd year of Biotechnology I already knew, that it's not what I like the most and what I want to do in the future. This year I was learning only subjects that were interesting for me and I decided to give up this degree and try to get on to Inter-faculty Individual Studies in Mathematics and Natural Sciences with main field in Psychology."
+},
+{
+    "date": "2009-03",
+    "content": "In the meantime I was developing my HTML and CSS skills and I did my second personal website - <a href='https://neuroskoki.pl' target='_blank'>NeuroSkoki</a>. It's a blog about neurobiology and skydiving which was my passion that time. The website was made initially in HTML and CSS from scratch and then I moved it to WordPress."
+},
+{
+    "date": "2009-10",
+    "content": "I started my desired field of study. In free time I was learning SEO optimization to have more users on my website."
+},
+{
+    "date": "2010-02",
+    "content": "While working at skydiving school Atmosfera, I created and was operating on my own, an online skydiving store on the eCommerce24h platform."
+},
+{
+    "date": "2012-03",
+    "content": "Since that time I have been working in IT industry. I found a job as SEO Copywriter and then as Digital Marketing Amplification Specialist."
+},
+{
+    "date": "2012-09",
+    "content": "I started a job in Dominus Digital Agency. I was building websites on CMS WordPress (adjusting themes to graphic design, configuring plugins etc.) which let me develop my CSS and basic PHP skills. I was also doing SEO optimization and Google AdWords campaigns."
+},
+{
+    "date": "2014-01",
+    "content": "I passed the Python course at the university."
+},
+{
+    "date": "2014-06",
+    "content": "I passed the C++ course at the university."
+},
+{
+    "date": "2014-10",
+    "content": "After graduating from the university I found a job in Alivia Oncology Foundation. During 5,5 years my duties were changing and were very diverse. I was initially working mainly in SEO optimization, then I started to do Facebook and Google ads and in later years I was coordinating online projects and cooperating with software houses. In the meantime I had other duties like recruitment of volunteers, editing oncology articles, having the role of Data Protection Officer."
+},
+{
+    "date": "2019-03",
+    "content": "Although I liked the feeling of doing something important which I had while working in oncology foundation, from some time I knew that I need the change and that my job duties are not what I want to do anymore. In March I decided to become front-end developer and from this time I have been learning intensively in my free time."
+},
+{
+    "date": "2019-10",
+    "content": "After few months of learning in the evenings and weekends I understood that I would need very long time to learn enough to find a dreamed job. I submitted resignation from work to be able to focus only on learning."
+},
+{
+    "date": "2020-02",
+    "content": "I quited my full time job and now I am developing my skills more intensively. I also work a bit as a freelancer. In recent months I did my first website on CMS Joomla (<a href='https://martazamarska.pl' target='_blank'>martazamarska.pl</a>) and first WordPress theme from scratch (<a href='https://odnawianie-mebli.com.pl' target='_blank'>odnawianie-mebli.com.pl</a>)."
+},
+{
+    "date": "2020-04",
+    "content": "I started a job as a Junior Front-End Developer in Mayeryn and I work in React.js."
+},
+{
+    "date": "Now",
+    "content": "I'm still looking for additional or one full time job which would let me develop my skills. I'm eager to work in React or WordPress but also to learn new technologies. My aim is also to move from Warsaw to some city closer to mountains, that's why, I'm looking for a job in Kraków, Wrocław, Upper Silesia or remote."
+}
+ ]
---
+++ scripts.js
@@ -0,0 +1,51 @@
+const container = document.getElementById("container");
+const timeline = document.getElementById("timeline");
+let data_num = 0;
+
+window.addEventListener("scroll", () => {
+  /*const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
+  
+  console.log(scrollTop, scrollHeight, clientHeight);
+	
+	if(clientHeight + scrollTop >= scrollHeight - 5) {
+    console.log("top");
+		getData();
+  }*/
+  if(window.scrollY + window.innerHeight >= document.documentElement.scrollHeight) {
+    getData();
+  }
+});
+
+function getData() {
+  if(data_num < data.length-1) {
+    addLifeEventToDOM(data[data_num]);
+    data_num++;
+  }
+  else if(data_num == data.length-1) {
+    addNowToDOM(data[data_num]);
+    data_num++;
+  }
+}
+
+function addLifeEventToDOM(data) {
+	const eventItem = document.createElement("div");
+	eventItem.classList.add("life-event");
+  eventItem.innerHTML = `
+      <div class="timeline-circle"></div>
+      <div class="content">
+          <div class="date">${data.date}</div>
+          <p>${data.content}</p>
+      </div>
+  `;
+	container.appendChild(eventItem);
+}
+
+function addNowToDOM(data) {
+  const nowItem = document.createElement("div");
+  nowItem.classList.add("now-container");
+  nowItem.innerHTML = `
+    <div class="now">${data.date}</div>
+    <div class="now-content">${data.content}</div>
+  `;
+  timeline.insertAdjacentElement("afterend", nowItem);
+}
